f67651009e86f592133099f197b50f2f9b630ac6
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2022-08-31 09:59:52 +0700

    video-26

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -26,8 +26,16 @@ let postCRUD = async (req, res) => {
   return res.send("post crud from server");
 };
 
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  return res.render("displayCRUD.ejs", {
+    data: data,
+  });
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
 };
