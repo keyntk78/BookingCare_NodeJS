4af796178c738fd19bdfc6c0f67c8e412d419004
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2022-08-31 11:14:36 +0700

    video-27

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -33,9 +33,30 @@ let displayGetCRUD = async (req, res) => {
   });
 };
 
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+
+    return res.render("editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("User not found!");
+  }
+};
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  await CRUDService.updateUserData(data);
+  return res.redirect("/get-crud");
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
 };
